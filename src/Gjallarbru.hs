3bc1b88d75ba0c39dc16df3466128f6253511831
Bryan Richter <b@chreekat.net>
2017-04-17 11:17:41 -0700

    start some tests

--- src/Gjallarbru.hs
+++ src/Gjallarbru.hs
@@ -2,12 +2,27 @@ module Gjallarbru where
 
 import Data.List.NonEmpty
 
+{-
+
+The syntax for templates is:
+
+ splice   := Templ1 value Templ2
+ value    := Var | accessor
+ accessor := Accessor Var | Accessor Grp1 value Grp2
+
+ Templ1 := '#{'
+ Templ2 := '}'
+ Grp1   :=  '('
+ Grp2   := ')'
+
+ Esc := '#\{'
+
+-}
+
 parse :: String -> ParseResult ()
 parse "#{" = ParseErr (UnclosedSplice 1 3 :| [])
 parse "#{}" = ParseSuccess ()
 parse _ = ParseSuccess ()
 
-
 data ParseResult a = ParseErr (NonEmpty ParseError) | ParseSuccess a deriving (Eq, Show)
-
 data ParseError = UnclosedSplice Int Int deriving (Eq, Show)
--- test/spec.hs
+++ test/spec.hs
@@ -5,7 +5,10 @@ import Test.Tasty.HUnit
 import Gjallarbru
 
 main :: IO ()
-main = defaultMain $ testGroup "specs"
-        [ testCase "reports unclosed splice" $ parse "#{" @?= ParseErr (UnclosedSplice 1 3 :|[])
+main = defaultMain $ testGroup "parse errors"
+        [ testCase "reports unclosed splice" $ parse "#{" @?= ParseErr (UnclosedSplice 1 3 :| [])
         , testCase "parses empty splice" $ parse "#{}" @?= ParseSuccess ()
+        , testCase "parses " $ pure ()
         ]
+
+-- What do I want to test? At the top, there are parse errors!
